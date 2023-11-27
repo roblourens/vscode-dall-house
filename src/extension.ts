aaaceeb88d4877f0d4bc73866fb777559ea7f596
Rob Lourens <roblourens@gmail.com>
2023-11-26 22:46:38 -0600

    Random selection of scene and art styles

--- src/extension.ts
+++ src/extension.ts
@@ -23,9 +23,13 @@ class DallClockWebviewProvider implements vscode.WebviewViewProvider {
 	private _refreshTimer: NodeJS.Timeout | undefined;
 	private _isRefreshing: boolean = false;
 
+	private _outputChannel: vscode.OutputChannel;
+
 	constructor(
 		private readonly _extensionContext: vscode.ExtensionContext,
-	) { }
+	) {
+		this._outputChannel = vscode.window.createOutputChannel('Dall Clock', { log: true });
+	}
 
 	private _register(disposable: vscode.Disposable) {
 		this._extensionContext.subscriptions.push(disposable);
@@ -88,8 +92,10 @@ class DallClockWebviewProvider implements vscode.WebviewViewProvider {
 		try {
 			this._isRefreshing = true;
 			vscode.commands.executeCommand('setContext', 'dall-clock.refreshing', true);
-			const url = await getAiImage(this._extensionContext, getPrompt());
-			console.log(url);
+			const prompt = getPrompt();
+			this._outputChannel.appendLine(`Prompt: ${prompt}`);
+			const url = await getAiImage(this._extensionContext, prompt);
+			this._outputChannel.appendLine(`Result: ${url}`);
 			this._lastImage = url;
 			this.setImage(url);
 			this._lastRefresh = now;
@@ -136,16 +142,107 @@ function getNonce() {
 	return text;
 }
 
-
 function getPrompt() {
-	const location = vscode.workspace.getConfiguration('dall-clock').get('location', '');
+	const location = vscode.workspace.getConfiguration('dall-clock').get('location', 'Seattle, WA');
 	const date = new Date();
 	const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 	const formattedDate = date.toLocaleDateString([], { month: 'long', day: 'numeric' });
+	const timeOfDay = getTimeOfDay(date);
+	const words = getWordsPart()
+		.replace('{time}', `"${time}"`)
+		.replace('{location}', `"${location}"`);
+	const scene = getScenePart().replace('{location}', `"${location}"`);
 	if (location) {
-		return `The location "${location}" at the time "${formattedDate}". There is a digital clock in the foreground which reads the _exact_ text "${time}". Capture the ambiance and details of the scene, including the lighting, surroundings, and any distinctive elements of this place and time.`;
+		return `${scene} at ${timeOfDay}. ${words} ${getArtStyleAndFeelPart()}`;
 	} else {
 		return `A digital clock which reads "${time}".`;
 		// return `A scene at the time "${formattedDate}". There is a digital clock in the foreground which reads "${time}". Capture the ambiance and details of the scene, including the lighting, surroundings, and any distinctive elements of this time.`;
+	}
+}
+
+const scenes = [
+	'A busy street scene in {location}',
+	'A cafe in {location}',
+	'A bar in {location}',
+	'A band performing on stage in {location}',
+	'A skyline view of {location}',
+	'A view of a famous landmark in {location}',
+	'The typical food eaten in {location}',
+	'{location}',
+];
+
+function getScenePart() {
+	return scenes[Math.floor(Math.random() * scenes.length)];
+}
+
+const artStyles = [
+	'pixel art',
+	'oil painting',
+	'watercolor painting',
+	'abstract painting',
+	'digital painting',
+	'3d rendering',
+	'political cartoon',
+	'modern comic book',
+	'classic comic book',
+	'photo',
+	'collage',
+	'charcoal sketch',
+	'tilt-shift photography',
+	'psychedlic art',
+	'ukiyo-e',
+	'',
+];
+
+const feels = [
+	'vaporwave',
+	'post-apocalyptic',
+	'sci-fi',
+	'steampunk',
+	'memphis group',
+	'optimistic',
+	'gloomy',
+	'utopian',
+	'dieselpunk',
+	'afrofuturism',
+	'cyberpunk',
+	''
+];
+
+function getArtStyleAndFeelPart(): string {
+	const artStyle = artStyles[Math.floor(Math.random() * artStyles.length)];
+	const feel = feels[Math.floor(Math.random() * feels.length)];
+	if (artStyle) {
+		return feel ? `Art style: ${artStyle} with a ${feel} feel.`
+		: `Art style: ${artStyle}.`;
+	} 
+
+	return feel ? `Has a ${feel} feel.` : ``;
+}
+
+const timeWordsOptions = [
+	'There is a digital clock in the foreground which shows the _exact_ text {time} and no other text.',
+	'There is a nixie tube clock in the foreground which shows the _exact_ text {time} and no other text.',
+	'The text {time} is overlayed on the image in large font.',
+	'Some object in the scene has the exact text {time} written on it in large font.',
+	'A person from {location} is holding a sign with the exact text {time} written on it in large font.',
+	'There is a large sign in the foreground which shows the exact text {time} and no other text.',
+];
+
+function getWordsPart(): string {
+	const randomIndex = Math.floor(Math.random() * timeWordsOptions.length);
+	return timeWordsOptions[randomIndex];
+}
+
+function getTimeOfDay(d: Date) {
+	const hour = d.getHours();
+	if (hour < 6) {
+		return 'night';
+	} else if (hour < 12) {
+		return 'morning';
+	} else if (hour < 18) {
+		return 'afternoon';
+	} else {
+		return 'evening';
 	}
 }
